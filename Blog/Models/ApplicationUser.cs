bf014d34e3da9caa2a0321f3ac6879c063dbcc0e
asiku <asiku@DESKTOP-TDJ219H>
2023-01-09 11:07:15 +0600

    Add project files.

---
+++ Blog/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Models
+{
+    public class Applicationuser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public List<Post>? Posts { get; set; }
+
+    }
+}
